d5ed94092533d825217c7882a2208a4358245010
Sk Zehad <sehabkhanzehad@yahoo.com>
2026-01-08 17:40:24 +0600

    feat: Make 'number' and 'biller_name' fields nullable in bills table migration

---
+++ database/migrations/2026_01_08_112630_make_number_and_biller_name_nullable_in_bills_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bills', function (Blueprint $table) {
+            $table->string('number')->nullable()->change();
+            $table->string('biller_name')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bills', function (Blueprint $table) {
+            $table->string('number')->nullable(false)->change();
+            $table->string('biller_name')->nullable(false)->change();
+        });
+    }
+};
